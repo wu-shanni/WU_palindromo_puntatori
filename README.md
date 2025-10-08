8cba89f441d54bf6fa7aed74ddd7257dace44c05
12999314 <12999314@itisrossi.vi.it>
2025-10-08 11:23:14 +0200

    Es1_Palindroma

---
+++ README.md
@@ -0,0 +1,10 @@
+Scrivete un programma che legga un messaggio e poi controlli se questo è
+palindromo (le lettere del messaggio sono le stesse sia leggendo da sinistra a
+destra che da destra a sinistra):
+Enter a message: He lived as a devil, eh?
+Palindrome
+Enter a message: Madam, I am Adam
+Not a palindrome
+Ignorate tutti i caratteri che non sono lettere. il programma utilizza dei
+puntatori invece che degli interi per tenere traccia delle posizioni all’interno del
+vettore[100]
--- main.c
+++ main.c
@@ -1,6 +1,43 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char vettore[100] = "i topi non avevano nipoti";
+    char *start;
+    char *end;
+    char *l;
+    char *senzaspazio;
+
+    l= vettore;
+    senzaspazio = vettore;
+
+    while (*l != '\0' && *l != '\n') {
+        if (isalpha(*l)) {
+            *senzaspazio = *l;
+            senzaspazio++;
+        }
+        l++;
+    }
+    *senzaspazio = '\0';
+
+
+    start = vettore;
+    end = vettore;
+
+    while (*end != '\0') {
+        end++;
+    }
+    end--;
+
+    while (start < end) {
+        if (*start != *end) {
+            printf("Non è palindroma");
+            return 0;
+        }
+        start++;
+        end--;
+    }
+
+    printf("E' palindroma");
     return 0;
 }
